3a30a37a32625f66b6439ee68cf4e0160730b1c6
Martin Berlusconi <martin.berlusconi@allata.com>
2026-08-24 17:56:02 -0300

    add tests

--- models/staging/stg_transactions.sql
+++ models/staging/stg_transactions.sql
@@ -5,15 +5,18 @@ MODEL (
   ),
   start '2026-08-17',
   cron '@daily',
-  audits (assert_valid_transactions) 
+  audits (assert_valid_transactions)
 );
 
+WITH source_data AS (
+  SELECT * FROM read_csv_auto('sources/transactions/transactions_*.csv')
+)
 SELECT
   CAST(transaction_id AS INT) AS transaction_id,
   CAST(customer_id AS VARCHAR) AS customer_id,
   CAST(transaction_date AS DATE) AS transaction_date,
   CAST(amount AS DECIMAL(10,2)) AS amount,
   CAST(currency AS VARCHAR) AS currency
-FROM read_csv_auto('sources/transactions/transactions_*.csv')
+FROM source_data
 WHERE
   CAST(transaction_date AS DATE) BETWEEN @start_date AND @end_date;
